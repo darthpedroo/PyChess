b49a16f2f50232ee3b49ad814e4b8c4a4b0e3a9c
darthpedroo <pedrovillarino06@gmail.com>
2023-10-24 23:39:44 -0300

    Creted Tile Class and separated DrawTile from Board

--- board/Board.py
+++ board/Board.py
@@ -1,32 +1,14 @@
 import pygame
-import time
+from tile.Tile import Tile
 class Board:
     def __init__(self):
-        self.SquareSize = 50
-        self.Colors = [[0,0,0],[255,255,255]]
         self.rows = 9 #This is one more number cause the loop ends in one less (9 -> 8)
         self.columns = 9
-        self.LeftCorner = [100,100]
-    
-    def drawSquare(self,surface,col,row):
-        
-        if row%2 == 0:
-            tempColor = self.Colors[col%2 == 0]
-        else:
-            tempColor = self.Colors[col%2 == 1]
-            
-        pygame.draw.rect(surface, tempColor, pygame.Rect(self.LeftCorner[0]+(self.SquareSize*col), self.LeftCorner[1]+(self.SquareSize*row), self.SquareSize, self.SquareSize))
-        pygame.display.flip()
+        self.LeftCorner = [100,200]
     
     def drawBoard(self,surface):
         for row in range(1,self.rows):
             for col in range(1,self.columns):
-                self.drawSquare(surface, col,row)
-                
+                Tile().drawTile(surface,col,row,self.LeftCorner)
 
-
-        
-    
-    
-        
 
